56421bfc1a89e311f562dc43d55ee771ba97ce9d
KirillYakub <kirillyakubchinskiy@gmail.com>
2020-04-22 20:24:05 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(260, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(364, 586);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Необходимо вычислить";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 95);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2A(A+B)-3A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "где А и В - массивы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 641);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            //генерируем случайные координаты;
+            int A1 = random.Next(0, 2), B1 = random.Next(0, 2);
+            int A2 = random.Next(0, 2), B2 = random.Next(0, 2);
+            int A3 = random.Next(0, 2), B3 = random.Next(0, 2);
+            int A4 = random.Next(0, 2), B4 = random.Next(0, 2);
+            int A5 = random.Next(0, 2), B5 = random.Next(0, 2);
+            int A6 = random.Next(0, 2), B6 = random.Next(0, 2);
+            int A7 = random.Next(0, 2), B7 = random.Next(0, 2);
+            int A8 = random.Next(0, 2), B8 = random.Next(0, 2);
+            int A9 = random.Next(0, 2), B9 = random.Next(0, 2);
+
+            //пересылаем в конструкторы соотвественные данные;
+            Matrix A = new Matrix(new int[,]{ { A1, A2, A3 } , { A4, A5, A6 } , { A7, A8, A9 } });
+            Matrix B = new Matrix(new int[,] { { B1, B2, B3 }, { B4, B5, B6 }, { B7, B8, B9 } });
+           
+            richTextBox1.Text += ($"Матрица А:" + "\n");
+            richTextBox1.Text += ($"{(A)}" + " ");
+            richTextBox1.Text += ("\n" + "Матрица В:" + "\n");
+            richTextBox1.Text += ($"{(B)}" + " ");
+            richTextBox1.Text += ("\n" + "Сумма матриц:" + "\n");
+            richTextBox1.Text += ($"{(A + B)}" + " ");
+            richTextBox1.Text += ("\n" + "Удвоенная матрица А:" + "\n");
+            richTextBox1.Text += ($"{(2 * A)}" + " ");
+            richTextBox1.Text += ("\n" + "Утроенная матрица А:" + "\n");
+            richTextBox1.Text += ($"{(3 * A)}" + " ");
+            richTextBox1.Text += ("\n" + "Пример 2А*(А + В):" + "\n");
+            richTextBox1.Text += ($"{((2 * A) * (A + B))}" + " ");
+            richTextBox1.Text += ("\n" + "Финальный пример 2А*(А + В) - 3А:" + "\n");
+            richTextBox1.Text += ($"{((2 * A) * (A + B)) - (3 * A)}" + " " + "\n");
+
+            if (Matrix.Symmetry(A, B) == true)
+                richTextBox1.Text += "Матрица симметричная\n";
+            else
+                richTextBox1.Text += "Матрица не симметричная\n";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    class Matrix
+    {
+        private int row, col;
+        private int[,] matrix;
+
+        //конструкторы, принимающие соответственные данные и присваивающие значения этих данных полям класса;
+        public Matrix(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+            this.matrix = new int[row, col];
+        }
+
+        public Matrix(int[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public double this[int i, int j]
+        {
+            get { return matrix[i, j]; } set { matrix[i, j] = Convert.ToInt32(value); }
+        }
+
+        //представляем массив в виде строки для вывода в textbox;
+        public override string ToString()
+        {
+            string str = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    str += matrix[i, j].ToString() + "  ";
+                }
+                str += ("\n");
+            }
+
+            return str;
+        }
+
+        //оперыторы перегрузки, принимающие 2 массива;
+        public static Matrix operator +(Matrix A, Matrix B)
+        {
+            Matrix result = new Matrix(3,3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = A[i, j] + B[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator -(Matrix A, Matrix B)
+        {
+            Matrix result = new Matrix(3, 3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = A[i, j] - B[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            Matrix result = new Matrix(3, 3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = A[i, j] * B[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator *(int a, Matrix A)
+        {
+            Matrix result = new Matrix(3, 3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = A[i, j] * a;
+                }
+            }
+
+            return result;
+        }
+
+        //проверка на симметричность;
+        public static bool Symmetry(Matrix A, Matrix B)
+        {
+            int X1, X2, X3, X4, X5, X6, X7, X8, X9;
+            
+            X1 = Convert.ToInt32(((2 * A[0, 0]) * (A[0 , 0] + B[0, 0])) - (3 * A[0, 0]));
+            X2 = Convert.ToInt32(((2 * A[0, 1]) * (A[0, 1] + B[0, 1])) - (3 * A[0, 1]));
+            X3 = Convert.ToInt32(((2 * A[0, 2]) * (A[0, 2] + B[0, 2])) - (3 * A[0, 2]));
+            X4 = Convert.ToInt32(((2 * A[1, 0]) * (A[1, 0] + B[1, 0])) - (3 * A[1, 0]));
+            X5 = Convert.ToInt32(((2 * A[1, 1]) * (A[1, 1] + B[1, 1])) - (3 * A[1, 1]));
+            X6 = Convert.ToInt32(((2 * A[1, 2]) * (A[1, 2] + B[1, 2])) - (3 * A[1, 2]));
+            X7 = Convert.ToInt32(((2 * A[2, 0]) * (A[2, 0] + B[2, 0])) - (3 * A[2, 0]));
+            X8 = Convert.ToInt32(((2 * A[2, 1]) * (A[2, 1] + B[2, 1])) - (3 * A[2, 1]));
+            X9 = Convert.ToInt32(((2 * A[2, 2]) * (A[2, 2] + B[2, 2])) - (3 * A[2, 2]));
+
+            if(X1 == X3 && X3 == X9 && X9 == X7 && X7 == X1)
+            {
+                if(X4 == X2 && X2 == X6 && X6 == X8 && X8 == X4)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
